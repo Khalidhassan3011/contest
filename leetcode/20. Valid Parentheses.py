ce8efdefbad2da9b89bf65bd05c956e0283acf81
khalid <khalidhassan3011@gmail.com>
2022-01-23 18:32:51 +0600

    2nd attempt

--- leetcode/20. Valid Parentheses.py
+++ leetcode/20. Valid Parentheses.py
@@ -1,8 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        start = 0
-        while start < len(s) - 1:
-            if s[start] == "(" and s[start + 1] == ")" or s[start] == "{" and s[start + 1] == "}" or s[start] == "[" and s[start + 1] == "]":
-                return True
-            start += 2
-        return False
+        parentheses = {
+            "(": 0,
+            ")": 0,
+            "{": 0,
+            "}": 0,
+            "[": 0,
+            "]": 0,
+        }
+
+        for p in s:
+            parentheses[p] += 1
+
+        return parentheses["("] == parentheses[")"] and parentheses["{"] == parentheses["}"] and parentheses["["] == parentheses["]"]
+
