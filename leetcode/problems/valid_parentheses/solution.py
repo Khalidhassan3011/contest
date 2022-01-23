95229425474c53fb6e4eb7918772c9a511abc628
khalid <khalidhassan3011@gmail.com>
2022-01-23 13:01:41 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

---
+++ leetcode/problems/valid_parentheses/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        parentheses_open = ["(", "{", "["]
+        parentheses_close = [")", "}", "]"]
+
+        stack = []
+
+        for v in s:
+            if v in parentheses_open:
+                stack.append(v)
+            elif not stack or parentheses_open[parentheses_close.index(v)] != stack.pop():
+                return False
+
+        return stack == []
