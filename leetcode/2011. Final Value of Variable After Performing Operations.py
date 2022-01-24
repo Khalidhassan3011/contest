586f426ed71be489b3ca188820194577c8d706e4
khalid <khalidhassan3011@gmail.com>
2022-01-24 14:03:24 +0600

    faster than 44.58% of Python3

--- leetcode/2011. Final Value of Variable After Performing Operations.py
+++ leetcode/2011. Final Value of Variable After Performing Operations.py
@@ -2,6 +2,14 @@ from typing import List
 
 
 class Solution:
+    def method1(self, operations: List[str]) -> int:
+        count_pos_b = operations.count("++X")
+        count_pos_a = operations.count("X++")
+        count_neg_b = operations.count("--X")
+        count_neg_a = operations.count("X--")
+
+        return count_pos_a + count_pos_b - count_neg_b - count_neg_a
+
     def method2(self, operations: List[str]) -> int:
         count: int = 0
 
@@ -14,8 +22,8 @@ class Solution:
         return count
 
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        # return self.method1(operations)
-        return self.method2(operations)
+        return self.method1(operations)
+        # return self.method2(operations)
 
 
 s = Solution()
