65f35b33f776b8c9ae8bfdbef2ca8dd4a6c78c79
khalid <khalidhassan3011@gmail.com>
2022-01-25 00:55:11 +0600

    wrong answer

--- leetcode/870. Advantage Shuffle.py
+++ leetcode/870. Advantage Shuffle.py
@@ -1,21 +1,32 @@
+from typing import List
+
+
 class Solution:
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
         result = []
-        traverse_all = False
-        for num2 in nums2:
-            if traverse_all:
-                break
-            for index, num1 in enumerate(nums1):
-                if num1 > num2:
-                    if index == len(nums1) - 1:
-                        traverse_all = True
-                    result.append(num1)
-                    nums1.pop(index)
+        nums1_cpy = nums1.copy()
+        nums1_cpy.sort()
 
+        for num2 in nums2:
+            found = False
+            for index, num1 in enumerate(nums1_cpy):
+                if num1 > num2:
+                    found = True
+                    result.append(num1)
+                    nums1_cpy.pop(index)
+                    nums1.remove(num1)
                     break
 
-        if len(nums1) > 0:
-            nums1.sort()
-            return result + nums1
+            if not found:
+                nums1_cpy.remove(nums1[-1])
+                result.append(nums1.pop())
 
-        return result
+        return result + nums1
+
+
+s = Solution()
+print(s.advantageCount(nums1=[2, 7, 11, 15], nums2=[1, 10, 4, 11]))
+print(s.advantageCount(nums1=[12, 24, 8, 32], nums2=[13, 25, 32, 11]))
+print(s.advantageCount(nums1=[5621, 1743, 5532, 3549, 9581], nums2=[913, 9787, 4121, 5039, 1481]))
+# [1743,9581,5532,5621,3549]
+# [5621,5532,9581,1743,3549]
