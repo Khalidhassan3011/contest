97928c122b4d3e13b05e32ec09ad7103557abb45
khalid <khalidhassan3011@gmail.com>
2022-01-24 23:01:48 +0600

    faster than 41.88% of Python3

---
+++ leetcode/1221. Split a String in Balanced Strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        l, r, ans = 0, 0, 0
+
+        for v in s:
+            if v == "L":
+                l += 1
+            else:
+                r += 1
+
+            if l == r:
+                ans += 1
+
+        return ans
+
+
+s = Solution()
+print(s.balancedStringSplit("RLRRLLRLRL"))
+print(s.balancedStringSplit("RLLLLRRRLR"))
+print(s.balancedStringSplit("LLLLRRRR"))
