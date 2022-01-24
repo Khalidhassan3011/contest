1424bcb48ea75f692928511b433846b0af23094a
khalid <khalidhassan3011@gmail.com>
2022-01-24 16:40:36 +0600

    faster than 30.66% of Python3

--- leetcode/1108. Defanging an IP Address.py
+++ leetcode/1108. Defanging an IP Address.py
@@ -1,14 +1,22 @@
 class Solution:
 
+    # faster than 30.66% of Python3
     def method1(self, address: str) -> str:
-        pass
+        defang: str = ""
+        for l in address:
+            if l == ".":
+                defang += "[.]"
+            else:
+                defang += l
+
+        return defang
 
     # faster than 86.07% of Python3
     def method2(self, address: str) -> str:
         return address.replace(".", "[.]")
 
     def defangIPaddr(self, address: str) -> str:
-        return self.method2(address)
+        return self.method1(address)
 
 
 s = Solution()
