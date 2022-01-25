b23ca0ae71df0b62594b7176ef7209aa6b43fe0f
khalid <khalidhassan3011@gmail.com>
2022-01-25 14:42:15 +0600

    Runtime Error

---
+++ leetcode/848. Shifting Letters.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def shift(self, letter: str, index: int) -> chr:
+        index = ord(letter) + index
+        if index > 122:
+            index -= 122 + 97
+        return chr(index)
+
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        temp = list(s)
+        for digit_index, digit in enumerate(shifts):
+            for letter_index in range(digit_index+1):
+                temp[letter_index] = self.shift(temp[letter_index], digit)
+
+        return "".join(temp)
+
+
+s = Solution()
+print(s.shiftingLetters(s="abc", shifts=[3, 5, 9]))
+print(s.shiftingLetters(s="aaa", shifts=[1, 2, 3]))
+print(s.shiftingLetters(s="bad", shifts=[10, 20, 30]))
