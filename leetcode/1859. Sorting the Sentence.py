d832926ff58890c60b64b7603b068710ab513aaf
khalid <khalidhassan3011@gmail.com>
2022-01-25 11:58:59 +0600

    faster than 5.03% of Python3

---
+++ leetcode/1859. Sorting the Sentence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words = s.split(" ")
+        result = [None] * len(words)
+
+        for word in words:
+            result[int(word[-1])-1] = word[:-1]
+
+        return " ".join(result)
+
+
+s = Solution()
+print(s.sortSentence("is2 sentence4 This1 a3"))
+print(s.sortSentence("Myself2 Me1 I4 and3"))
