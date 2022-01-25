3a606620667f5c82a18d616e019825d4e5042d27
khalid <khalidhassan3011@gmail.com>
2022-01-25 07:03:04 +0000

    Sync LeetCode submission - Check if All A's Appears Before All B's (python3)

---
+++ leetcode/problems/check_if_all_a's_appears_before_all_b's/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkString(self, s: str) -> bool:
+        a, b = 0, 0
+        for letter in s:
+            if letter == "a":
+                a += 1
+                if b > 0:
+                    return False
+            else:
+                b += 1
+
+        return True
