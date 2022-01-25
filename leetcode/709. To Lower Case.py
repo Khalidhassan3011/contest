522fb417c35ce0a00914a2289e85349848739253
khalid <khalidhassan3011@gmail.com>
2022-01-25 13:23:49 +0600

    faster than 36.26% of Python3

---
+++ leetcode/709. To Lower Case.py
@@ -0,0 +1,10 @@
+class Solution:
+
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
+
+
+s = Solution()
+print(s.toLowerCase("Hello"))
+print(s.toLowerCase("here"))
+print(s.toLowerCase("LOVELY"))
