9c942d7bc7b88089240816e9d8a8d46e45eb759a
khalid <khalidhassan3011@gmail.com>
2022-01-25 12:01:17 +0000

    Sync LeetCode submission - Robot Return to Origin (python3)

---
+++ leetcode/problems/robot_return_to_origin/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        forward, side = 0, 0
+        for move in moves:
+            if move == 'U':
+                forward += 1
+            elif move == 'D':
+                forward -= 1
+            elif move == 'L':
+                side += 1
+            elif move == 'R':
+                side -= 1
+
+        return forward == 0 and side == 0
