926e289cb8c630ed271766b270f3f79299e654d1
khalid <khalidhassan3011@gmail.com>
2022-01-25 15:07:51 +0600

    Time Limit Exceeded

--- leetcode/848. Shifting Letters.py
+++ leetcode/848. Shifting Letters.py
@@ -3,9 +3,10 @@ from typing import List
 
 class Solution:
     def shift(self, letter: str, index: int) -> chr:
-        index = ord(letter) + index
+        index = ord(letter) + index % 26
         if index > 122:
-            index -= 122 + 97
+            index = (index - 122 - 1) + 97
+
         return chr(index)
 
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
@@ -13,7 +14,6 @@ class Solution:
         for digit_index, digit in enumerate(shifts):
             for letter_index in range(digit_index+1):
                 temp[letter_index] = self.shift(temp[letter_index], digit)
-
         return "".join(temp)
 
 
