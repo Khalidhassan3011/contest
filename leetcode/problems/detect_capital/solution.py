66ad3432e2a31aadcf5250a29c9624f244026098
khalid <khalidhassan3011@gmail.com>
2022-01-25 07:49:23 +0000

    Sync LeetCode submission - Detect Capital (python3)

---
+++ leetcode/problems/detect_capital/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper() or word.islower():
+            return True
+
+        count = 0
+        for letter in word:
+            if letter.isupper():
+                count += 1
+                if count > 1:
+                    return False
+
+        return word[0].isupper()
