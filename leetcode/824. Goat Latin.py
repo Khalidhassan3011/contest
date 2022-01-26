ae5623518956f74c3aff84f4cb9324b4aca794c8
khalid <khalidhassan3011@gmail.com>
2022-01-26 10:16:33 +0600

    Runtime: 41 ms, faster than 32.21% of Python3

---
+++ leetcode/824. Goat Latin.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def postfix(self, index) -> str:
+        return "ma" + "a" * (index + 1)
+
+    def reformat(self, word, index) -> str:
+        vowels = ["a", "e", "i", "o", "u"]
+        if word[0].lower() in vowels:
+            return word + self.postfix(index)
+        else:
+            return word[1:] + word[0] + self.postfix(index)
+
+    def toGoatLatin(self, sentence: str) -> str:
+        return " ".join([self.reformat(v, i) for i, v in enumerate(sentence.split())])
+
+
+s = Solution()
+print(s.toGoatLatin(sentence="I speak Goat Latin"))
+print(s.toGoatLatin(sentence="I speak Goat Latin") == "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")
+print(s.toGoatLatin(sentence="The quick brown fox jumped over the lazy dog"))
+print(s.toGoatLatin(sentence="The quick brown fox jumped over the lazy dog") == "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")
+
