d7cbd9c130b7072bc41a312c2977a0472fe0db7a
khalid <khalidhassan3011@gmail.com>
2022-01-29 02:08:07 +0600

    Runtime Error

--- leetcode/345. Reverse Vowels of a String.py
+++ leetcode/345. Reverse Vowels of a String.py
@@ -1,14 +1,25 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
-        value = {
-            "a": "u",
-            "u": "a",
-            "e": "o",
-            "o": "e",
-        }
+        letters = list(s)
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        vowel_index = []
 
-        return "".join(value[letter] if letter in value else letter for letter in s)
+        for i, v in enumerate(letters):
+            if v in vowels:
+                vowel_index.append(i)
+
+        while vowel_index:
+            temp = letters[vowel_index[0]]
+            letters[vowel_index[0]] = letters[vowel_index[len(vowel_index) - 1]]
+            letters[vowel_index[len(vowel_index) - 1]] = temp
+
+            vowel_index.pop()
+            vowel_index.pop(0)
+
+        return "".join(letters)
 
 
 print(Solution().reverseVowels(s="hello"))
-print(Solution().reverseVowels(s="hello") == "holle")
+print(Solution().reverseVowels(s="leetcode"))
+# print(Solution().reverseVowels(s="hello") == "holle")
+# print(Solution().reverseVowels(s="hello") == "leotcede")
