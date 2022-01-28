4058d4f3e2712780285f366370cea94686c7704c
khalid <khalidhassan3011@gmail.com>
2022-01-28 20:34:42 +0600

    Wrong Answer

---
+++ leetcode/1078. Occurrences After Bigram.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def findOcurrences(self, text: str, first: str, second: str) -> List[str]:
+        words = text.split()
+        first_match_index = words.index(first)
+
+        result = []
+
+        for i in range(first_match_index + 1, len(words), 1):
+            if words[i] != second:
+                result.append(words[i])
+                break
+
+        words.reverse()
+        second_match_index = words.index(second)
+        result.append(words[second_match_index - 1])
+
+        return result
+
+
+s = Solution()
+a = s.findOcurrences(text="alice is a good girl she is a good student", first="a", second="good")
+print(a)
+print(a == ["girl", "student"])
+
+a = s.findOcurrences(text="we will we will rock you", first="we", second="will")
+print(a)
+print(a == ["we", "rock"])
