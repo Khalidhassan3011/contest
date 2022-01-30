83ec2217bc4fbe6b9f6657a1c8839e6c687b74af
khalid <khalidhassan3011@gmail.com>
2022-01-30 09:31:53 +0600

    Runtime: 620 ms, faster than 11.22% of Python3

--- leetcode/485. Max Consecutive Ones.py
+++ leetcode/485. Max Consecutive Ones.py
@@ -3,22 +3,7 @@ from typing import List
 
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        s = "".join([str(i) for i in nums])
-        result = 0
-        count_sub = 0
-        index = 0
-
-        while index < len(s):
-            if index == 0 or s[index] != s[index - 1]:
-                count_sub = 1
-            else:
-                count_sub += 1
-
-            if count_sub > result:
-                result = count_sub
-            index += 1
-
-        return result
+        return len(max("".join([str(i) for i in nums]).split("0")))
 
 
 print(Solution().findMaxConsecutiveOnes(nums=[1, 1, 0, 1, 1, 1]))
