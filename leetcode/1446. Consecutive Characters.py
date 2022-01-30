cf57591f692b76f500104d2255a7a147f256f7b9
khalid <khalidhassan3011@gmail.com>
2022-01-30 09:06:36 +0600

    Runtime: 65 ms, faster than 8.00% of Python3

---
+++ leetcode/1446. Consecutive Characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxPower(self, s: str) -> int:
+        result = 0
+        count_sub = 0
+        index = 0
+
+        while index < len(s):
+            if index == 0 or s[index] != s[index - 1]:
+                count_sub = 1
+            else:
+                count_sub += 1
+
+            if count_sub > result:
+                result = count_sub
+            index += 1
+
+        return result
+
+
+print(Solution().maxPower("abbcccddddeeeeedcba"))
