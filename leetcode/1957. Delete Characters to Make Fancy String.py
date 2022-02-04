0fbdbe20f26eebcd1ac238bf73cf112a3a1f13c8
khalid <khalidhassan3011@gmail.com>
2022-02-04 19:05:47 +0600

    faster than 8.0% of Python3

---
+++ leetcode/1957. Delete Characters to Make Fancy String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        result = ""
+        index = 0
+
+        s_len = len(s)
+
+        while index < s_len:
+            sub = s[index]
+            if index < s_len - 1:
+                while s[index] == s[index + 1]:
+                    if len(sub) < 2:
+                        sub += s[index + 1]
+                    index += 1
+                    if index == s_len - 1:
+                        break
+
+            result += sub
+
+            index += 1
+
+        return result
+
+
+s = Solution()
+print(s.makeFancyString(s="leeetcode"))
+print(s.makeFancyString(s="aaabaaaa"))
+print(s.makeFancyString(s="aab"))
