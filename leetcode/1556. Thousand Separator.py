d09756f1ee9f910a234a48e7240a3c3b8e0ddc60
khalid <khalidhassan3011@gmail.com>
2022-02-06 10:21:24 +0600

    Runtime: 53 ms, faster than 15.30% of Python3

--- leetcode/1556. Thousand Separator.py
+++ leetcode/1556. Thousand Separator.py
@@ -2,11 +2,27 @@ class Solution:
     def thousandSeparator(self, n: int) -> str:
         s = str(n)
         s_len = len(s)
-        if s_len < 4:
-            return s
-        return s[:s_len - 3] + "." + s[-3:]
+        result = ""
+        index = s_len - 1
+
+        sub_count = 0
+
+        while index > -1:
+            result = s[index] + result
+            sub_count += 1
+            index -= 1
+
+            if sub_count == 3 and index > -1:
+                result = "." + result
+                sub_count = 0
+
+        return result
 
 
 s = Solution()
+print(s.thousandSeparator(n=987))
 print(s.thousandSeparator(n=987) == "987")
+print(s.thousandSeparator(n=1234))
 print(s.thousandSeparator(n=1234) == "1.234")
+print(s.thousandSeparator(n=123456789))
+print(s.thousandSeparator(n=123456789) == "123.456.789")
