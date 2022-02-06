223dd089a99a990349751032f5cc5068b3b22b1a
khalid <khalidhassan3011@gmail.com>
2022-02-06 09:58:45 +0600

    Wrong Answer

---
+++ leetcode/1576. Replace All ?'s to Avoid Consecutive Repeating Characters.py
@@ -0,0 +1,36 @@
+import string
+
+
+class Solution:
+    def pickLatter(self, pre: str, next: str):
+        for l in string.ascii_lowercase:
+            if pre != l and l != next:
+                return l
+
+    def modifyString(self, s: str) -> str:
+        result = ""
+        s_len = len(s)
+        for i in range(s_len):
+            if s[i] == "?":
+                if i == 0:
+                    result += self.pickLatter("", s[i])
+                else:
+                    result += self.pickLatter(result[-1], s[i + 1] if i != s_len - 1 else "")
+            else:
+                result += s[i]
+
+        return result
+
+
+s = Solution()
+a = s.modifyString(s="?zs")
+print(a)
+print(a == "azs")
+
+a = s.modifyString(s="ubv?w")
+print(a)
+print(a == "ubvaw")
+
+a = s.modifyString(s="j?qg??b")
+print(a)
+print(a == "jaqgacb")
