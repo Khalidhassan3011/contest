3df9c6d961243ba69d2ea806d4a0dc06e4159f8b
khalid <khalidhassan3011@gmail.com>
2022-02-09 09:48:21 +0600

    Runtime: 70 ms, faster than 48.13% of Python3

---
+++ leetcode/1002. Find Common Characters.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        words_len = len(words)
+        if words_len == 1:
+            return list(words[0])
+
+        letters = list(words[0])
+        check_words = words[1:]
+        words_len -= 1
+
+        result = ""
+        for l in letters:
+            letter_in_all_word = False
+            for word_index in range(words_len):
+                if l in check_words[word_index]:
+                    letter_in_all_word = True
+                    check_words[word_index] = check_words[word_index].replace(l, "", 1)
+                else:
+                    letter_in_all_word = False
+                    break
+            if letter_in_all_word:
+                result += l
+
+        return list(result)
+
+
+s = Solution()
+a = s.commonChars(words=["bella", "label", "roller"])
+print(a)
+print(a == ["e", "l", "l"])
+
+a = s.commonChars(words=["cool", "lock", "cook"])
+print(a)
+print(a == ["c", "o"])
