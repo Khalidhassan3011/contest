cb4b961d53a138400f0e3de6148e54b392b6638c
khalid <khalidhassan3011@gmail.com>
2022-02-10 09:13:07 +0600

    Runtime: 49 ms, faster than 31.41% of Python3

---
+++ leetcode/551. Student Attendance Record I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        return False if s.count("A") > 1 or s.count("LLL") > 0 else True
+
+
+s = Solution()
+a = s.checkRecord(s="PPALLP")
+print(a)
+print(a == True)
+
+a = s.checkRecord(s="PPALLL")
+print(a)
+print(a == False)
