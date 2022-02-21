6339a34c4c2d1a5c7f7711f4c48f228bd8bb338e
khalid <khalidhassan3011@gmail.com>
2022-02-21 14:48:25 +0600

    reformat

--- leetcode/easy/35. Search Insert Position.py
+++ leetcode/easy/35. Search Insert Position.py
@@ -37,27 +37,29 @@ class Solution:
                 return index
         return len(nums)
 
-        # nums_len = len(nums)
-        #
-        # if target < nums[0]: return 0
-        # if target > nums[nums_len - 1]: return nums_len
-        #
-        # start = 0
-        # end = nums_len - 1
-        #
-        # while start <= end:
-        #     middle = (end + start) // 2
-        #     current_num = nums[middle]
-        #
-        #     if current_num == target:
-        #         return middle
-        #     elif current_num > target:
-        #         end = middle - 1
-        #     else:
-        #         start = middle + 1
-        #
-        # return start
 
+"""
+       nums_len = len(nums)
+
+       if target < nums[0]: return 0
+       if target > nums[nums_len - 1]: return nums_len
+
+       start = 0
+       end = nums_len - 1
+
+       while start <= end:
+           middle = (end + start) // 2
+           current_num = nums[middle]
+
+           if current_num == target:
+               return middle
+           elif current_num > target:
+               end = middle - 1
+           else:
+               start = middle + 1
+
+       return start
+"""
 
 s = Solution()
 a = s.searchInsert(nums=[1, 3, 5, 6], target=5)
