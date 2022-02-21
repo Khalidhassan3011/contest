6f1191aec4a8e5f5b7cdebd801f38d841111e083
khalid <khalidhassan3011@gmail.com>
2022-02-21 14:44:17 +0600

    Runtime: 52 ms, faster than 78.29% of Python3

--- leetcode/easy/35. Search Insert Position.py
+++ leetcode/easy/35. Search Insert Position.py
@@ -31,26 +31,31 @@ from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        nums_len = len(nums)
+        for index, num in enumerate(nums):
+            if num == target or num > target:
+                return index
+        return len(nums)
 
-        if target < nums[0]: return 0
-        if target > nums[nums_len - 1]: return nums_len
-
-        start = 0
-        end = nums_len - 1
-
-        while start <= end:
-            middle = (end + start) // 2
-            current_num = nums[middle]
-
-            if current_num == target:
-                return middle
-            elif current_num > target:
-                end = middle - 1
-            else:
-                start = middle + 1
-
-        return start
+        # nums_len = len(nums)
+        #
+        # if target < nums[0]: return 0
+        # if target > nums[nums_len - 1]: return nums_len
+        #
+        # start = 0
+        # end = nums_len - 1
+        #
+        # while start <= end:
+        #     middle = (end + start) // 2
+        #     current_num = nums[middle]
+        #
+        #     if current_num == target:
+        #         return middle
+        #     elif current_num > target:
+        #         end = middle - 1
+        #     else:
+        #         start = middle + 1
+        #
+        # return start
 
 
 s = Solution()
