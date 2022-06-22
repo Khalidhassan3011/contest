b9e27741688b6b36352ff437ab2ad34a03d078cf
khalid <khalidhassan3011@gmail.com>
2022-06-22 08:24:25 +0000

    Sync LeetCode submission - Two Sum (python3)

--- leetcode/problems/two_sum/solution.py
+++ leetcode/problems/two_sum/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        visited = {}
-        for index, value in enumerate(nums):
-            if target - value in visited:
-                return [visited[target - value], index]
-            
-            visited[value] = index
+        alreadyExist = {}
+        
+        for index, num in enumerate(nums):
+            try:
+                return [alreadyExist[target - num], index]
+            except:
+                alreadyExist[num] = index
