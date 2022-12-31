27de0cfa7ff259759869e07dae4335fce69ba350
khalid <khalidhassan3011@gmail.com>
2022-12-31 07:04:28 +0000

    Sync LeetCode submission - Ransom Note (python3)

---
+++ leetcode/problems/ransom_note/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in set(ransomNote):
+            if ransomNote.count(char) > magazine.count(char):
+                return False
+        
+        return True
