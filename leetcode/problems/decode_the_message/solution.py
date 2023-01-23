743419a75fb11e474655b23de6d3f40b4a8cf8b1
khalid <khalidhassan3011@gmail.com>
2023-01-23 17:46:57 +0000

    Sync LeetCode submission - Decode the Message (python3)

--- leetcode/problems/decode_the_message/solution.py
+++ leetcode/problems/decode_the_message/solution.py
@@ -2,42 +2,26 @@ from collections import OrderedDict
 
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
+        combine = {}
+        letter = 97
+        for char in key:
+            if len(combine) == 26:
+                break
+            if char == " ":
+                continue
 
-
-        # METHOD 1
-
-        # letters = {}
-
-        # # ascii value of a
-        # lastLetterInsert = 97 
-
-        # for keyCharecter in key:
-        #     if keyCharecter == " ":
-        #         continue
-
-        #     if keyCharecter not in letters:
-        #         letters[keyCharecter] = chr(lastLetterInsert)
-        #         lastLetterInsert += 1
+            if char not in combine:
+                combine[char] = chr(letter)
+                letter += 1
             
-        #     if len(letters) == 26 or lastLetterInsert == 123:
-        #         break;
 
-        # result = ""
-        # for charecter in message:
-        #     if charecter == " ":
-        #         result += charecter
-        #     else:
-        #         result += letters[charecter]
+        result = ""
+        for char in message:
+            result += combine.get(char,  " ")
         
-        # return result
+        return result
 
 
-
-        # METHOD 2
-
-        decode = dict(zip("".join(OrderedDict.fromkeys(key.replace(" ",""))), "abcdefghijklmnopqrstuvwxyz"))
-
-        return "".join(decode.get(c, " ") for c in message)
 
         
 
